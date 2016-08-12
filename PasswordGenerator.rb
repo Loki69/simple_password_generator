d4cf6094d315721570404137962a9e3260a7a2f1
Garanin Pavel <mur069@yandex.ru>
2016-08-12 00:24:19 -0400

    add new method

--- PasswordGenerator.rb
+++ PasswordGenerator.rb
@@ -1,11 +1,25 @@
-def generate(count=7)
+def get_password(count=7)
 password=""
 count.times do
-password+=Random.rand(65..122).chr
+password+=generate
 end
 puts password
 end
 
+def generate()
+random = Random.rand(48..122)
+case random
+when 48..57
+  random.chr
+when 65..90
+  random.chr
+when 97..122
+  random.chr
+else
+  return generate
+end
+end
+
 if __FILE__ == $0
-generate
+get_password
 end
