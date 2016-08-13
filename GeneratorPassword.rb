c34c1423b27223170913738841da5eb359d39fa2
Garanin Pavel <mur069@yandex.ru>
2016-08-13 12:01:01 -0400

    password generation algorithm change

--- GeneratorPassword.rb
+++ GeneratorPassword.rb
@@ -1,23 +1,47 @@
 class GeneratorPassword
 
-  def get_password(count=7)
+  CONSONANT="bcdfghjklmnpqrstvwxz"
+  TWO_CONSONANT = ["kl","lk","st","str","sp","pr","scr","dr","kr","tr","vr"]
+  VOWEL="aaaeeeiioouuy"
+
+  def generate_password(count=7)
     password=""
-    count.times do
-      password+=generate
+    i=0
+    while password.length < count
+      case i%2
+      when 0
+        password += add_random_count_consonant(password, count)
+      else
+        password += get_vowel
+      end
+      i += 1
     end
-    puts password
+    password
   end
 
-  def generate()
-    random = Random.rand(48..122)
-    case random
-    when 48..57
-      random.chr
-    when 65..90
-      random.chr
-    when 97..122
-      random.chr
+  private
+
+  def  add_random_count_consonant(password,count)
+    if password.length + 2 < count
+      random_number_to(1) == 0 ? get_consonant : get_two_consonant
     else
-      return generate
+      get_consonant
     end
   end
+
+  def random_number_to(count=1)
+    Random.rand(0..count)
+  end
+
+  def get_consonant
+    CONSONANT[random_number_to(CONSONANT.length-1)]
+  end
+
+  def get_two_consonant
+    TWO_CONSONANT[random_number_to(TWO_CONSONANT.length-1)]
+  end
+
+  def get_vowel
+    VOWEL[random_number_to(VOWEL.length-1)]
+  end
+end
--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
+require_relative 'GeneratorPassword'
+
 if __FILE__ == $0
-  c.b(a)
   puts "Enter length password"
   count = gets.chomp
-  GeneratorPassword.get_password(count.to_i)
+  password_generat = GeneratorPassword.new
+  password = password_generat.generate_password count.to_i
+  puts password
 end
